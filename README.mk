6a05019a091dd7d7f5d58a193687ea76a619a2a7
Alexandre Girard <alx.girard@gmail.com>
2010-03-09 13:29:11 +0100

    add readme

---
+++ README.mk
@@ -0,0 +1,41 @@
+# SimplePress : All in One Wordpress
+
+After repeating installation of Wordpress with the same plugin, 
+I've made this flavored-install that covers most of my needs.
+
+## Installation
+
+* Please refer to Wordpress manual to install normally the system
+* Activate the plugin you need from the Plugin Administation Panel
+
+## Plugins
+
+These following plugins are installed by default, you'll just need to activate them
+if you need them:
+
+* [All-in-One SEO Pack](http://wordpress.org/extend/plugins/all-in-one-seo-pack/)
+* [Contact Form 7](http://wordpress.org/extend/plugins/contact-form-7/)
+* [WPtouch iPhone Theme](http://wordpress.org/extend/plugins/wptouch/)
+* [Google XML Sitemaps](http://wordpress.org/extend/plugins/google-sitemap-generator/)
+* [WP-PageNavi](http://wordpress.org/extend/plugins/wp-pagenavi/)
+* [e-Commerce](http://getshopped.org/)
+
+## Languages
+
+Because I'm french, and that I've lived some time in Spain, I've also include the
+languages files for French and Spanish versions of Wordpress.
+
+To keep track of the translation:
+
+* [fr_FR](http://www.wordpress-fr.net/telechargements)
+* [es_ES](http://www.todowp.org/wordpress-en-castellano/)
+
+## Todo
+
+* More themes by default, it could include new themes each months from [Smashing Magazine](www.smashingmagazine.com)
+* Wordpress-Mu flavored installation
+* Specific use-cases using git branched: ecommerce, portfolio, static page, ...
+
+## Contact
+
+[Alexandre Girard](http://alexgirard.com)
